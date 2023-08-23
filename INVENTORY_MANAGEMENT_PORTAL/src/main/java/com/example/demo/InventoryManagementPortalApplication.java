1b47500952f7ed4e360a6033474da34af5543207
priya25270 <108729878+priya25270@users.noreply.github.com>
2023-08-23 10:00:08 +0530

    Add files via upload

---
+++ INVENTORY_MANAGEMENT_PORTAL/src/main/java/com/example/demo/InventoryManagementPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementPortalApplication.class, args);
+	}
+
+}
